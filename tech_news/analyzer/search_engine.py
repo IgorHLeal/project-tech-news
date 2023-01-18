26bf2ef9089b12d1c0bb1e0f94dbaff5069c7934
Igor Leal <igorhenriqueleal3@gmail.com>
2023-01-18 07:31:19 -0300

    feat: Implementando a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news_list = search_news(query)
+    news_tuple = []
+
+    for new in news_list:
+        new_search = (new['title'], new['url'])
+        news_tuple.append(new_search)
+    return news_tuple
 
 
 # Requisito 7
